16e7551c1c96e89db245fd70504e96a0a8e1e8a4
Foxtail-Pip <ren.isaac.green@gmail.com>
2025-10-16 12:07:10 +0100

    Movement and Raycast working

--- GAM405/Assets/Input/PlayerController.cs
+++ GAM405/Assets/Input/PlayerController.cs
@@ -1,14 +1,22 @@
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Playables;
 
 public class PlayerController : MonoBehaviour
 {
     public InputActionReference moveAction;
     public float moveSpeed = 5f;
+    public float jumpForce = 5f;
     private Rigidbody2D rb;
     private Vector2 moveInput;
-  
+    [SerializeField] bool isGrounded = true;
+    [SerializeField] private bool activeAbility = false;
+    [SerializeField] float groundCheckDistance = 5f;
+
+    enum CurrentState { CharacterA, CharacterB, CharacterC, AntigravActive, AttackActive, ShrinkActive, Jumping }
+    [SerializeField] CurrentState state = CurrentState.CharacterA;
+
     // Update is called once per frame
     void Update()
     {
@@ -16,12 +24,16 @@ public class PlayerController : MonoBehaviour
         {
             moveInput = moveAction.action.ReadValue<Vector2>();
         }
+        HandleState();
+        // Jump(); If this goes here the second I'm grounded it thinks I'm jumping state
     }
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.interpolation = RigidbodyInterpolation2D.Interpolate;
     }
+
     private void OnEnable()
     {
         if (moveAction != null) moveAction.action.Enable(); 
@@ -29,13 +41,62 @@ public class PlayerController : MonoBehaviour
     }
     private void OnDisable()
     {
-        if (moveAction !=null) moveAction.action.Disable(); 
+        if (moveAction != null) moveAction.action.Disable();
     }
+
     private void FixedUpdate()
     {
         if (moveInput.sqrMagnitude > 1) moveInput.Normalize();
 
-        Vector2 step = moveInput * moveSpeed * Time.fixedDeltaTime;
-        rb.MovePosition(rb.position + step);
+        float stepx = moveInput.x * moveSpeed * Time.fixedDeltaTime;
+        Vector2 step = new Vector2(stepx, 0);
+        rb.MovePosition(rb.position + step); //This is left/right movement
+
+        RaycastHit2D raycastHit;
+        raycastHit = Physics2D.Raycast(transform.position + (Vector3.down * 0.51f), Vector2.down, groundCheckDistance);
+        Debug.DrawRay(transform.position, Vector2.down, Color.red, groundCheckDistance); //This makes the laser appear
+       if (raycastHit.collider != null && raycastHit.collider.gameObject.layer != 3)
+        {
+            Debug.Log(raycastHit.collider.gameObject);
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        } 
+
+       
+    }
+
+    public void Jump()
+    {
+        // if (Input.GetKeyDown(KeyCode.Space)) 
+        if (rb == null) return;
+        if (!isGrounded) return;
+
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        isGrounded = false;
+        state = CurrentState.Jumping;
+    }
+
+    private void HandleState()
+    {
+        switch (state)
+        {
+            case CurrentState.CharacterA:
+                break;
+            case CurrentState.CharacterB:
+                break;
+            case CurrentState.CharacterC:
+                break;
+            case CurrentState.AntigravActive:
+                break;
+            case CurrentState.AttackActive: 
+                break;
+            case CurrentState.ShrinkActive:
+                break;
+            case CurrentState.Jumping:
+                break;
+        }
     }
 }
---
+++ GAM405/Assets/Scripts/Pseudocode.cs
@@ -0,0 +1,77 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+using UnityEngine.UIElements;
+using static UnityEditor.VersionControl.Asset;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+
+public class Pseudocode : MonoBehaviour
+{
+    /* 
+ ON START
+ currentSprite = A //Sprites are like costumes for the player object
+ currentState = CharacterA //State will make sure you don’t use an ability with the wrong character
+ activeAbility = false //This check makes sure you aren’t changing character while still using an ability
+
+
+ SWITCH CHARACTER
+ If activeAbility = true, disable activeAbility
+ If activeAbility = false
+ On mouse pressed, change sprite by 1
+ If currentSprite is A, change to B
+     currentState = CharacterB
+ If currentSprite is B, change to C
+     currentState = CharacterC
+ If currentSprite is C, change to A
+     currentState = CharacterA
+
+
+ ANTIGRAV
+ On[ability button] pressed
+ If currentSprite is A and activeAbility = false
+     ‘Fall’ to ceiling(transform.translate?)
+ currentState = AntigravActive
+ activeAbility = true
+ If currentSprite is A and activeAbility = true
+     ‘Fall’ to floor
+     currentState = CharacterA
+     activeAbility = false
+
+
+ ATTACK
+ On[ability button] pressed
+ If currentSprite is B and activeAbility = false
+     Detect collision
+     Destroy all objects within collision field marked breakable
+ currentState = AttackActive
+ activeAbility = true
+ If currentSprite is B and activeAbility = true //find timed way to get out of AttackActive
+     currentState = Character B
+     activeAbility = false
+
+
+ SHRINK
+ On[ability button] pressed
+ If currentSprite is C and activeAbility = false
+     Object transform.scale 0.5
+     Collision transform.scale 0.5
+ currentState = ShrinkActive
+ activeAbility = true
+ If currentSprite is C and activeAbility = true
+     Object transform.scale 1
+     Collision transform.scale 1
+     currentState = Character C
+     activeAbility = false
+
+     STATES
+ CharacterA
+ CharacterB
+ CharacterC
+ AntigravActive
+ AttackActive
+ ShrinkActive
+ Jumping
+
+    */
+}
